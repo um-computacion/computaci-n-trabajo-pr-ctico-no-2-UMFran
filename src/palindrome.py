4a5d6094478f738951910693248f1f05eff55f23
francisco martín <frga.martin@alumno.um.edu.ar>
2025-04-13 20:57:09 -0300

    Tercer issue del segundo milestone, implementacion de la comparacion de caracteres

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,2 +1,7 @@
 def is_palindrome(word):
     word = word.lower().replace(" ", "").replace(".", "").replace(",", "").replace(";", "").replace(":", "").replace("?", "").replace("!", "")
+
+    if len(word) <= 1:
+        return True
+    else:
+        return word[0] == word[-1] and is_palindrome(word[1:-1])
